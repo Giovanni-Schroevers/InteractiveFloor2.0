352522ab6ea6d9db497ca244a3e96468bf35c1d5
81179 <81179@glr.nl>
2018-04-09 12:25:39 +0200

    Werk van 09/04

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public Transform vis;
+    private Vector3 vel;
+    private float Direction = 3;
+    public float curTime = 0;
+
+    void Start() {
+        SetVel();
+    }
+
+    void Update() {
+        if (curTime < Direction) {
+            curTime += Time.deltaTime;
+        }
+        else
+        {
+            SetVel();
+            if (Random.Range(0, 1) > 0.5f)
+            {
+                Direction += Random.Range(0, 1);
+            }
+            else {
+
+            }
+        }
+    }
+
+    private void SetVel(){
+        if (Random.Range(0, 1) > 0.5f)
+        {
+            vel.x = 10 * 10 * Random.Range(0, 1);
+        }
+        else {
+            vel.x = -10 * -10 * Random.Range(0, 1);
+        }
+
+        if (Random.Range(0, 1) > 0.5f)
+        {
+            vel.z = 10 * 10 * Random.Range(0, 1);
+        }
+        else
+        {
+            vel.z = -10 * -10 * Random.Range(0, 1);
+        }
+    }
+}
